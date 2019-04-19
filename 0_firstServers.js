3f60eeeab3179a43988f88170aedfac60cc176bb
vbruhn <30861405+vbruhn@users.noreply.github.com>
2019-04-19 21:50:01 +0200

    Update 0_firstServers.js

--- 0_firstServers.js
+++ 0_firstServers.js
@@ -3,24 +3,21 @@ hackscript = "_hack_me.script";
 
 i = 0;
 while (i < servers.length) {
+    serverHackLevel = getServerRequiredHackingLevel(servers[i]);
+    
     //Wait for player to reach the correct hacking level
-    while (getHackingLevel() < getServerRequiredHackingLevel(servers[i])) {
+    while (getHackingLevel() < serverHackLevel) {
         sleep(20000);
     }
 
     //Copy our generic hacking script and weaken script over to the target server
     scp(hackscript, servers[i]);
-    
+
     //NUKE the target server to gain root access
     nuke(servers[i]);
     
     //Execute our scripts on the target server
-    if (servers[i] == "joesguns") {
-        exec(hackscript, servers[i], 4, servers[i], 50000000, 10);
-    } else {
-        exec(hackscript, servers[i], 4, servers[i], 2000000, 10);
-    }
-    exec(weakenscript, servers[i], 1, servers[i]);
+    exec(hackscript, servers[i]);
 
     ++i;    
 }
