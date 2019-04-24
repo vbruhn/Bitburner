2d85daf6da7d7133a7123bc6ad67320bb2eb0e4a
vbruhn <30861405+vbruhn@users.noreply.github.com>
2019-04-24 20:33:40 +0200

    Update 5_purchaseServers.js

--- 5_purchaseServers.js
+++ 5_purchaseServers.js
@@ -1,14 +1,12 @@
-hackScripts = ["_singleHack.script", "_singleGrow.script", "_singleWeaken.script", "_serverScripts.script", "best_target.txt"];
+hackScripts = ["_singleHack.script", "_singleGrow.script", "_singleWeaken.script", "_serverScripts.script", "best_target.txt", "_permaWeaken.script"];
 
 serverCostMulti = 55000;
 maxMoney = getServerMoneyAvailable('home') / 25;
+maxRam = getPurchasedServerMaxRam('home');
 
 // get servers with the minimum, predefined amount, but check if there is already
 // a higher configured pserv
 ramToBuy = 16;
-//if (serverExists('pserv-0')) {
-//    ramToBuy = Math.max(ramToBuy, getServerRam('pserv-0')[0]);
-//}
 
 // calculate max RAM for the buck
 if (maxMoney > serverCostMulti * ramToBuy) {
@@ -20,6 +18,10 @@ if (maxMoney > serverCostMulti * ramToBuy) {
     ramToBuy /= 2;
 }
 
+if (ramToBuy > maxRam) {
+    ramToBuy = maxRam;
+}
+
 //ramToBuy = 64;
 
 print('Calculated: ' + ramToBuy + ' GB RAM for ' + serverCostMulti * ramToBuy + '$');
@@ -28,31 +30,39 @@ i = 0;
 while (i < getPurchasedServerLimit()) {
     if (getServerMoneyAvailable("home") > getPurchasedServerCost(ramToBuy)) {
         if (!serverExists('pserv-' + i) || getServerRam('pserv-' + i)[0] < ramToBuy) {
-            if (maxMoney > serverCostMulti * ramToBuy) {
-                if (serverExists('pserv-' + i)) {
-                    killall('pserv-' + i);
-                    sleep(20 * 1000, false);
-                    deleteServer('pserv-' + i);
-                    sleep(5 * 1000, false);
-                }
+            //if (maxMoney > serverCostMulti * ramToBuy) {
+            if (serverExists('pserv-' + i)) {
+                killall('pserv-' + i);
+                sleep(8000);
+                deleteServer('pserv-' + i);
+                sleep(5000);
+            }
 
-                if (!serverExists('pserv-' + i)) {
-                    hostname = purchaseServer('pserv-' + i, ramToBuy);
-                    if (hostname) {
-                        for (j = 0; j < hackScripts.length; ++j) {
-                            scp(hackScripts[j], hostname);
-                        }
-                        exec('_serverScripts.script', hostname);
-                        
-                        print('Bought player server #' + i + ' with ' + ramToBuy + ' GB RAM for $' + serverCostMulti * ramToBuy);
-                        ++i;
+            if (!serverExists('pserv-' + i)) {
+                print("Line 49");
+                hostname = purchaseServer('pserv-' + i, ramToBuy);
+                if (hostname) {
+                    for (j = 0; j < hackScripts.length; ++j) {
+                        scp(hackScripts[j], hostname);
                     }
+                    // run only the permaWeakenScript on pserv-0
+                    if (i === 0) {
+                        pmemory = getServerRam(hostname);
+                        pavailableMem = pmemory[0] - pmemory[1];
+                        pnumThreads = Math.round(pavailableMem / getScriptRam("_permaWeaken.script", hostname) - 1);
+                        exec("_permaWeaken.script", hostname, pnumThreads);
+                    } else {
+                        exec('_serverScripts.script', hostname);
+                    }
+                    print('Bought player server #' + i + ' with ' + ramToBuy + ' GB RAM for $' + serverCostMulti * ramToBuy);
+                    ++i;
                 }
             }
-            sleep (500);
+            //}
+            sleep(500);
         } else {
             ++i;
-            sleep (500);
+            sleep(500);
         }
     }
     sleep(6000);
