8fe402322ee3df0d5db160366449a7435a4f9161
vbruhn <30861405+vbruhn@users.noreply.github.com>
2019-04-26 13:25:43 +0200

    Update _serverScripts.js
    Updated the Thread-Number Calculation and added a few comments

--- _serverScripts.js
+++ _serverScripts.js
@@ -8,7 +8,6 @@ hackScript = "_singleHack.script";
 
 // Data of the Server, this script is running on
 serverName = getHostname();
-serverRam = getServerRam(serverName);
 
 // get the RAM-Load of each script
 weakenMem = getScriptRam(weakenScript, serverName);
@@ -24,98 +23,127 @@ clientMinSecurity = clientData[5];
 money_target = Math.round(clientMaxMoney / 1000) * 900;
 sec_target = (clientMinSecurity - 0) + 5; //-0 to convert string to number
 
-availRAM = serverRam[0] - serverRam[1]; // available RAM - used RAM
 
 while (true) {
+    // refresh memory-usage
+    serverRam = getServerRam(serverName);
+    availRAM = serverRam[0] - serverRam[1]; // available RAM - used RAM
+
     //check the stats of our Client
     currentSecLevel = Math.round(getServerSecurityLevel(clientName));
     currentDollars = Math.round(getServerMoneyAvailable(clientName));
-    
+
     cD = currentDollars.toLocaleString(
-        undefined,   // leave undefined to use the browser's locale,
-                    // or use a string like 'en-US' to override it.
-        {minimumFractionDigits: 0});
-    
+        undefined, // leave undefined to use the browser's locale,
+        // or use a string like 'en-US' to override it.
+        { minimumFractionDigits: 0 });
+
     wD = money_target.toLocaleString(
-        undefined,   // leave undefined to use the browser's locale,
-                    // or use a string like 'en-US' to override it.
-        {minimumFractionDigits: 0});
-    
+        undefined, // leave undefined to use the browser's locale,
+        // or use a string like 'en-US' to override it.
+        { minimumFractionDigits: 0 });
+
     print("Current Sec-Lvl: " + currentSecLevel + " of max. " + sec_target);
     print("Current Money: " + cD + " / " + wD);
 
     if (currentSecLevel > sec_target) {
-        numThreads = (availRAM / 3 / weakenMem); // Will be rounded to nearest integer by run()-Function
-
-        waitTime = Math.round(getWeakenTime(clientName));
-        print("Weaken() will last for approx. " + waitTime + " seconds.");
-        
-        if (!scriptRunning(weakenScript, serverName)) {
-            run(weakenScript, numThreads, clientName);
+        // calculate the max number of threads for the weaken-schript:
+        // if the grow-script is still running (for the sake of XP), then
+        // use all of the left RAM ... else use only half of it.
+        if (scriptRunning(growScript, serverName)) {
+            numThreads = Math.min(availRAM / weakenMem);
+        } else {
+            // delete the "/ 2" if you want to use all available RAM
+            numThreads = Math.min(availRAM / 2 / weakenMem);
         }
 
-        // wait for the script to start
-        sleep(10000);
-        while (scriptRunning(weakenScript, serverName)) {
-            sleep(1000);
-            
-            // check if another script has already lowered the secLevel
-            currentSecLevel = Math.round(getServerSecurityLevel(clientName));
-            if (currentSecLevel <= sec_target) {
-                print ("secLevel reached goal by external script!");
-                break;
+        if (numThreads > 0) {
+            waitTime = Math.round(getWeakenTime(clientName));
+            print("Weaken() will last for approx. " + waitTime + " seconds.");
+
+            if (!scriptRunning(weakenScript, serverName)) {
+                run(weakenScript, numThreads, clientName);
+            }
+
+            // wait for the script to start
+            sleep(10000);
+            while (scriptRunning(weakenScript, serverName)) {
+                sleep(1000);
+
+                // check if another script has already lowered the secLevel
+                currentSecLevel = Math.round(getServerSecurityLevel(clientName));
+                if (currentSecLevel <= sec_target) {
+                    print("secLevel reached goal by external script!");
+                    break;
+                }
             }
         }
-        
+
     } else if (currentDollars < money_target) {
-        numThreads = (availRAM / 3 / growMem); // Will be rounded to nearest integer by run()-Function
-
-        waitTime = Math.round(getGrowTime(clientName));
-        print("Grow() will last for approx. " + waitTime + " seconds.");
-        
-        if (!scriptRunning(growScript, serverName)) {
-            run(growScript, numThreads, clientName);
+        // calculate the max number of threads for the growth-schript:
+        // if the weaken-script is still running (for the sake of XP), then
+        // use all of the left RAM ... else use only half of it.
+        if (scriptRunning(weakenScript, serverName)) {
+            numThreads = Math.min(availRAM / growMem);
+        } else {
+            // delete the "/ 2" if you want to use all available RAM
+            numThreads = Math.min(availRAM / 2 / growMem);
         }
 
-        // wait for the script to start
-        sleep(10000);
-        while (scriptRunning(growScript, serverName)) {
-            sleep(1000);
-            
-            // check if another script has reached the growAmount
-            currentDollars = Math.round(getServerMoneyAvailable(clientName));
-            if (currentDollars > money_target) {
-                print("Money reached goal by external script!");
-                break;
+        if (numThreads > 0) {
+            waitTime = Math.round(getGrowTime(clientName));
+            print("Grow() will last for approx. " + waitTime + " seconds.");
+
+            if (!scriptRunning(growScript, serverName)) {
+                run(growScript, numThreads, clientName);
+            }
+
+            // wait for the script to start
+            sleep(10000);
+            while (scriptRunning(growScript, serverName)) {
+                sleep(1000);
+
+                // check if another script has reached the growAmount
+                currentDollars = Math.round(getServerMoneyAvailable(clientName));
+                if (currentDollars > money_target) {
+                    print("Money reached goal by external script!");
+                    break;
+                }
             }
         }
-        
     } else {
-        // use this to empty the client
-        maxThreads = (availRAM / 3 / hackMem);
+        // on low RAM-servers, you might want to change the divisor to 1.1 
+        // or the like
+        maxThreads = Math.min(availRAM / 2 / hackMem);
 
         // or steal a percentage
         wantedMoney = Math.round(money_target / 5);
         numThreads = hackAnalyzeThreads(clientName, wantedMoney);
-        print("Want $" + wantedMoney + " ...");
+
 
         if (maxThreads < numThreads) {
             numThreads = maxThreads;
         }
-        numThreads=Math.round(numThreads);
+        numThreads = Math.round(numThreads);
 
-        waitTime = Math.round(getHackTime(clientName));
-        print("Hack(" + numThreads + " Threads) will last for approx. " + waitTime + " seconds.");
+        if (numThreads > 0) {
+            waitTime = Math.round(getHackTime(clientName));
 
-        run(hackScript, numThreads, clientName);
+            print("Want $" + wantedMoney + " ...");
+            print("Hack(" + numThreads + " Threads) will last for approx. " + waitTime + " seconds.");
 
-        // wait for the script to start
-        sleep(10000);
-        while (scriptRunning(hackScript, serverName)) {
-            // TODO: check if another script has already hacked the server
-            sleep(1000);
+            run(hackScript, numThreads, clientName);
+
+            // wait for the script to start
+            sleep(10000);
+            while (scriptRunning(hackScript, serverName)) {
+                // TODO: check if another script has already hacked the server
+                sleep(1000);
+            }
+        } else {
+            print("Waiting for other scripts to finish.");
         }
     }
-    print (" ");
+    print(" ");
     sleep(1000);
 }
