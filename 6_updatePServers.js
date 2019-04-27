c13427c9aa964df67e89773d9e17ba59014fc286
vbruhn <30861405+vbruhn@users.noreply.github.com>
2019-04-27 21:38:14 +0200

    Update 6_updatePServers.js

--- 6_updatePServers.js
+++ 6_updatePServers.js
@@ -1,24 +1,40 @@
 export async function main(ns) {
-    const hackScripts = ["_singleHack.ns", "_singleGrow.ns", "_singleWeaken.ns", 
-        "_serverScripts.ns", "best_target.txt", "_permaWeaken.ns"];
+    const hackScripts = ["_singleHack.ns", "_singleGrow.ns", "_singleWeaken.ns",
+        "_serverScripts.ns", "best_target.txt", "_permaWeaken.ns"
+    ];
 
     ns.tprint("Sending Scripts and Target to P-Servers!");
 
     var i = 0;
+    var target = "";
+    var memory = 0;
+    var availableMem = 0;
+    var numThreads = 0;
+
+    // kill all processes
     while (i < ns.getPurchasedServerLimit()) {
-        var target = "pserv-" + i;
+        target = "pserv-" + i;
 
         if (ns.serverExists(target)) {
             ns.killall(target);
-            await ns.sleep(6000);
+        }
+        ++i;
+    }
+    await ns.sleep(6000);
+
+    i = 0;
+    while (i < ns.getPurchasedServerLimit()) {
+        target = "pserv-" + i;
+
+        if (ns.serverExists(target)) {
 
             for (let j = 0; j < hackScripts.length; ++j) {
                 ns.scp(hackScripts[j], target);
             }
             if (i === 0) {
-                var memory = ns.getServerRam(target);
-                var availableMem = memory[0] - memory[1];
-                var numThreads = Math.round(availableMem / ns.getScriptRam("_permaWeaken.ns", target) - 1);
+                memory = ns.getServerRam(target);
+                availableMem = memory[0] - memory[1];
+                numThreads = Math.round(availableMem / ns.getScriptRam("_permaWeaken.ns", target) - 1);
                 await ns.exec("_permaWeaken.ns", target, numThreads);
             } else {
                 await ns.exec("_serverScripts.ns", target);
