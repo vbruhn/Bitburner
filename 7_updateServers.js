39a9db332c5bc5f1d5a595d36973f3b7ef4293b8
vbruhn <30861405+vbruhn@users.noreply.github.com>
2019-04-28 13:41:53 +0200

    performance tweaks

--- 7_updateServers.js
+++ 7_updateServers.js
@@ -2,20 +2,29 @@ export async function main(ns) {
     var hackScripts = ["best_target.txt", "_permaWeaken.ns"];
 
     var nukedServers = ns.read("nukedServers.txt").split(",");
+    var target = "";
+    var memory = 0;
+    var availableMem = 0;
     
     for (let i = 0; i < nukedServers.length; ++i) {
         if (nukedServers[i].length < 1) { break; }
-        
-        var target = nukedServers[i];
+
+        target = nukedServers[i];
         await ns.killall(target);
-        while (ns.scriptRunning(hackScripts[1], target)) { await ns.sleep(150); }
+    }
+    while (ns.scriptRunning(hackScripts[1], target)) { await ns.sleep(150); }
+
+    for (let i = 0; i < nukedServers.length; ++i) {
+        if (nukedServers[i].length < 1) { break; }
         
+        target = nukedServers[i];
+
         for (let j = 0; j < hackScripts.length; ++j) {
             ns.scp(hackScripts[j], target);
         }
 
-        var memory = ns.getServerRam(target);
-        var availableMem = memory[0] - memory[1];
+        memory = ns.getServerRam(target);
+        availableMem = memory[0] - memory[1];
         if (availableMem > 8) {
             var numThreads = Math.round(availableMem / ns.getScriptRam("_permaWeaken.ns", target) - 1);
             await ns.exec("_permaWeaken.ns", target, numThreads);
