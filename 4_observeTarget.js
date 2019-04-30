d9f94893637a7aa3ab62081731d5f0b20a17eba5
vbruhn <30861405+vbruhn@users.noreply.github.com>
2019-04-30 21:42:45 +0200

    Added CSS ...
    ... for a nicer log ;-)

--- 4_observeTarget.js
+++ 4_observeTarget.js
@@ -11,23 +11,25 @@ export async function main(ns) {
 
     var a = 0,
         b = 0,
-        c = 0;
+        c = 0,
+        d = 0;
     var currentMoney = "",
         maxMoney = "",
-        moneyGain = "";
+        moneyGain = "",
+        cashPerSec = "";
     var timeStart = Math.round(ns.getTimeSinceLastAug() / 1000);
     var timeNow = 0,
         moneyNow = 0;
-    var port = ns.getPortHandle(1);
+    const port = ns.getPortHandle(1);
     var output = "";
 
     while (true) {
         timeNow = Math.round(ns.getTimeSinceLastAug() / 1000) - timeStart;
-        ns.print("Observing: " + clientName);
 
         a = Math.round(ns.getServerSecurityLevel(clientName));
         b = Math.round(ns.getServerMoneyAvailable(clientName));
         c = Math.round(ns.getServerMoneyAvailable(thisServer) - moneyStart);
+        d = Math.round(c / timeNow * 100) / 100;
 
         currentMoney = b.toLocaleString(
             undefined, // leave undefined to use the browser's locale,
@@ -41,17 +43,26 @@ export async function main(ns) {
             undefined, // leave undefined to use the browser's locale,
             // or use a string like 'en-US' to override it.
             { minimumFractionDigits: 0 });
+        cashPerSec = d.toLocaleString(
+            undefined, // leave undefined to use the browser's locale,
+            // or use a string like 'en-US' to override it.
+            { minimumFractionDigits: 2 });
             
         if (!port.empty()) {
             output = port.read();
-            port.clear();
+            //port.clear();
         }
-
-        ns.print(" ");
-        ns.print("Home-Server's Task: " + output);
-        ns.print("Security-Level: " + a + " of wanted " + clientMinSecurity);
-        ns.print("Available Money: $" + currentMoney + " / $" + maxMoney);
-        ns.print("Money-Delta: $" + moneyGain + " in " + timeNow + " seconds.");
+        const rowStart = `<div class="row"><div class="col-3" style="text-align:right;color:#3D505D;">`; // ${rowStart}
+        const newCol = `</div><div class="col-9"  style="text-align:left;color:#8DA0AD;">`; // ${newCol}
+        const rowEnd = `</div></div>`; // ${rowEnd}
+        
+        var txt =   `<h1 style="text-align:center;color:#3D505D;">Observing: ${clientName}</h1>` + 
+                    `${rowStart}Home-Server's Task:${newCol}${output}${rowEnd}` +
+                    `${rowStart}Security-Level:${newCol}${a} of wanted ${clientMinSecurity}${rowEnd}` +
+                    `${rowStart}Available Money:${newCol}$${currentMoney} / ${maxMoney}${rowEnd}` + 
+                    `${rowStart}Money-Delta:${newCol}$${moneyGain} in ${timeNow} seconds.${rowEnd}` +
+                    `${rowStart}Money-Delta:${newCol}$${cashPerSec} per second.${rowEnd}`;
+        ns.print(txt);
 
         await ns.sleep(1000);
         ns.clearLog();
