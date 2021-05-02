fb5a1ef738dc301ef7215c3110b251c062843b85
vbruhn <30861405+vbruhn@users.noreply.github.com>
2021-05-02 10:01:55 +0200

    Create 00_firstServers.js

---
+++ 00_firstServers.js
@@ -0,0 +1,31 @@
+export async function main(ns) {
+    const servers = ["foodnstuff", "sigma-cosmetics", "joesguns", "nectar-net", "hong-fang-tea", "harakiri-sushi"];
+    const hackscript = "_hack_me.ns";
+    var numThreads = 0;
+
+    var i = 0;
+    while (i < servers.length) {
+        var serverHackLevel = ns.getServerRequiredHackingLevel(servers[i]);
+
+        //Wait for player to reach the correct hacking level
+        while (ns.getHackingLevel() < serverHackLevel) {
+            await ns.sleep(10000);
+        }
+
+        //Copy our generic hacking script and weaken script over to the target server
+        ns.scp(hackscript, servers[i]);
+        var svRam = ns.getServerRam(servers[i]);
+        ns.print(servers[i] + ":");
+        var svRamAvail = svRam[0] - svRam[1];
+        ns.print(svRamAvail + " RAM available")
+
+        //NUKE the target server to gain root access
+        await ns.nuke(servers[i]);
+
+        //Execute our scripts on the target server
+        numThreads = Math.floor(svRamAvail / ns.getScriptRam("_hack_me.ns", servers[i]));
+        await ns.exec(hackscript, servers[i], numThreads);
+
+        ++i;
+    }
+}
